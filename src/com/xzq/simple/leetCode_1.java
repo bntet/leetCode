7698d2b2a9edac61643196a3d051d0512c563a65
xuzhiqian <1012305134@qq.com>
2022-04-08 23:14:08 +0800

    Initial commit

---
+++ src/com/xzq/simple/leetCode_1.java
@@ -0,0 +1,15 @@
+package com.xzq.simple;
+
+public class leetCode_1 {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        return null;
+    }
+
+    public void main(String[] args) {
+        int[] nums = new int[]{2,7,11,15};
+        int[] intList = this.twoSum(nums, 9);
+        System.out.println(intList);
+    }
+}
