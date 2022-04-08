0142961311576934d20d13e42937b8678395e9fd
bntet <1012305134@qq.com>
2022-04-09 00:03:02 +0800

    leetCode_1 update

--- src/com/xzq/simple/leetCode_1.java
+++ src/com/xzq/simple/leetCode_1.java
@@ -3,13 +3,20 @@ package com.xzq.simple;
 public class leetCode_1 {
 
     public static int[] twoSum(int[] nums, int target) {
-
+        for (int num : nums) {
+            for (int num2 : nums) {
+                int targer2 = target;
+                if((num + num2) == targer2){
+                    return new int[]{num, num2};
+                }
+            }
+        }
         return null;
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         int[] nums = new int[]{2,7,11,15};
-        int[] intList = this.twoSum(nums, 9);
-        System.out.println(intList);
+        int[] intList = twoSum(nums, 9);
+        System.out.println(intList[0] + "," + intList[1]);
     }
 }
